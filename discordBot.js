481e35e7e5a148ab7a38a8af7586ebe3176fbc80
IroiKanta <e261.whydunit.62305@gmail.com>
2023-05-31 22:20:50 +0900

    ♻️ refuctor / 今後の機能実装に備えた改修

--- discordBot.js
+++ discordBot.js
@@ -1,11 +1,49 @@
 function main() {
-  // discord側で作成したボットのウェブフックURL
-  const discordWebHookURL = PropertiesService.getScriptProperties().getProperty('webhookurl_1');
+  // const を try-catch で使うため
+  const properties = (() => {
+    try {
+        return PropertiesService.getScriptProperties().getProperties()
+    } catch (err) {
+        console.log('Failed with error %s', err.message);
+        return "";
+    }
+  })();
 
-  // 投稿するチャット内容と設定
+  // 変数の準備
+  let webhookurl = properties['webhookurl_1'];
+  let webhookurl2 = properties['webhookurl_2'];
+  let jsonfolder = properties['json_folder_id'];
+
+  sendMessage(webhookurl);
+  sendMessage(webhookurl2);
+}
+
+function setUrl(){
+  for(num in content_set){
+    for(numnum in content_set[num]){
+      Logger.log('test num: %s', numnum);
+    }
+  }
+}
+
+function loadJson(){
+  var contents = DriveApp.getFolderById(jsonfolder)
+  .getFilesByName('test.txt')
+  .next()
+  .getBlob()
+  .getDataAsString("sjis");
+  Logger.log(contents);
+}
+
+/**
+ * 指定した URL にメッセージを送信
+ * @param {string} webhookurl - webhook url
+ */
+function sendMessage(webhookurl) {
+  // 投稿する内容と設定
   const message = {
     "content": "Hello!", // チャット本文
-    "tts": false  // ロボットによる読み上げ機能を無効化
+    "tts": false  // text to speach
   }
 
   const param = {
@@ -14,5 +52,9 @@ function main() {
     "payload": JSON.stringify(message)
   }
 
-  UrlFetchApp.fetch(discordWebHookURL, param);
+  try {
+    UrlFetchApp.fetch(webhookurl, param);
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
 }
---
+++ settings.js
@@ -0,0 +1,20 @@
+/**
+ * 送信先の URL と送信するファイルの組み合わせをセットする
+ */
+const content_set = {
+    "1" : {
+      "webhook_key" : "webhookurl_1",
+      "filename" : {
+        "1" : "test1.txt",
+        "2" : "test2.txt"
+      } 
+    },
+    "2" : {
+      "webhook_key" : "webhookurl_2",
+      "filename" : {
+        "1" : "test1.txt",
+        "2" : "test2.txt"
+      }
+    }
+  };
+  
